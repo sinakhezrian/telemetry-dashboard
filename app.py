5536f8adb5a38dc47653058af4382838d6dc99eb
sinakhezrian <sinakhezryann@gmail.com>
2025-10-15 13:23:39 +0200

    chore[.env, app.py]: set basic configs

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
+import os
+from dotenv import load_dotenv 
 from flask import Flask, render_template
 
+load_dotenv()
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['DATABASE_URL'] = os.getenv('DATABASE_URL')  
 
 @app.route('/')
 def dashboard():
